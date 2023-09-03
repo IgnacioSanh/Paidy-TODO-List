21db6f181648447b5f919e76416d325ec6324e73
Ignacio Sanhueza <ignacio.sanhueza.l@gmail.com>
2023-09-03 01:27:12 -0300

    Revert "Expo local authentication"

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,3 @@
 module.exports = {
-  presets: ['babel-preset-expo', '@babel/preset-typescript'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        alias: {
-          '~components': './src/components',
-        },
-      },
-    ],
-  ],
+  presets: ['module:metro-react-native-babel-preset'],
 };
