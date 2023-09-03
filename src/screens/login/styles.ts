7ac3896115bd95031c478990ec577e9e078e76d1
Ignacio Sanhueza <ignacio.sanhueza.l@gmail.com>
2023-09-03 20:04:32 -0300

    Check security screen. Add custom font. Started theme files

---
+++ src/screens/login/styles.ts
@@ -0,0 +1,13 @@
+import {StyleSheet} from 'react-native';
+import {Spacings} from '~theme/spacings';
+
+const styles = StyleSheet.create({
+  flexEnd: {
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    marginBottom: Spacings.large * 2,
+    flex: 1,
+  },
+});
+
+export default styles;
