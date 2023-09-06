5fae909bb158cb608fee861ee40671a38b0cd1be
Ignacio Sanhueza <ignacio.sanhueza.l@gmail.com>
2023-09-06 00:17:22 -0300

    Added store persistance with MMKV

--- babel.config.js
+++ babel.config.js
@@ -7,6 +7,7 @@ module.exports = {
         alias: {
           '~components': './src/components',
           '~contexts': './src/contexts',
+          '~hooks': './src/hooks',
           '~screens': './src/screens',
           '~theme': './src/theme',
           '~types': './src/types',
